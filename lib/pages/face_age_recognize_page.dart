1e59fb757b1312024eb0342799a2226c7d1a0d4a
cildhdi <miles_yang@foxmail.com>
2019-05-09 23:46:56 +0800

    pick image

--- lib/pages/face_age_recognize_page.dart
+++ lib/pages/face_age_recognize_page.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FaceAgeRecognizePage extends StatefulWidget {
   final nickName;
@@ -10,13 +12,73 @@ class FaceAgeRecognizePage extends StatefulWidget {
 }
 
 class _FaceAgeRecognizePageState extends State<FaceAgeRecognizePage> {
+  File _image;
+
+  Future getImage(ImageSource source) async {
+    var image = await ImagePicker.pickImage(source: source);
+    setState(() {
+      _image = image;
+    });
+  }
+
+  Future uploadImage(BuildContext context) async {
+    debugPrint("dhsidhas");
+    Scaffold.of(context).showSnackBar(SnackBar(
+      content: const Text("未选择图片"),
+      duration: Duration(seconds: 2),
+    ));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("面部年龄识别"),
       ),
-      body: Container(),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                mainAxisSize: MainAxisSize.max,
+                children: <Widget>[
+                  RaisedButton(
+                    child: const Text("相机拍照"),
+                    onPressed: () {
+                      getImage(ImageSource.camera);
+                    },
+                  ),
+                  Container(
+                    width: 20.0,
+                  ),
+                  RaisedButton(
+                    child: const Text("相册选择"),
+                    onPressed: () {
+                      getImage(ImageSource.gallery);
+                    },
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 40.0,
+            ),
+            _image == null ? const Text('未选择图片') : Image.file(_image),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.file_upload),
+          onPressed: () async {
+            Scaffold.of(context).showSnackBar(SnackBar(
+              content: const Text("未选择图片"),
+              duration: Duration(seconds: 2),
+            ));
+          }),
     );
   }
 }
